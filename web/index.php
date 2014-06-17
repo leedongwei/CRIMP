ddee8144dab5f1d827e6770f49d87813319aca8b
leedongwei <leedongwei@gmail.com>
2014-06-17 18:30:28 +0800

    Implemented navbar

---
+++ web/index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" 		content="width=device-width">
+		<meta name="description" 	content="">
+		<meta name="keywords" 		content="">
+		<meta name="author"			content="NUS Climbing Club">
+		<!--<meta name="google-site-verification" content="ahvU7TBTn8QeZIp_Ayts3MTG9v14kNXw5cbLE8t9lU4" />
+		<link rel="shortcut icon" href="img/nusclimb.ico"> -->
+
+		<title>
+			NUS Climbing
+		</title>
+
+		<!-- Bootstrap core CSS -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+
+		<!-- Custom styles for this template -->
+		<link href="css/style.css" rel="stylesheet">
+
+	</head>
+
+	<body>
+
+
+
+		<div class="navbar navbar-default navbar-fixed-top">
+			<div class="container">
+				<div class="navbar-header">
+			    <a class="navbar-brand" href="#">NUS-BD Boulderactive</a>
+			    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+			      <span class="icon-bar"></span>
+			      <span class="icon-bar"></span>
+			      <span class="icon-bar"></span>
+			    </button>
+			  </div>
+			  <div class="navbar-collapse collapse">
+			    <ul class="nav navbar-nav">
+						<li>Novice Men <span class="nav-links">
+							<a href=".php">Qualifying</a> |
+							<a href=".php">Finals</a>
+						</span></li>
+						<li>Novice Women <span class="nav-links">
+							<a href=".php">Qualifying</a> |
+							<a href=".php">Finals</a>
+						</span></li>
+						<li>Intermediate Men <span class="nav-links">
+							<a href=".php">Qualifying</a> |
+							<a href=".php">Finals</a>
+						</span></li>
+						<li>Intermediate Women <span class="nav-links">
+							<a href=".php">Qualifying</a> |
+							<a href=".php">Finals</a>
+						</span></li>
+						<li>Open Men <span class="nav-links">
+							<a href=".php">Qualifying</a> |
+							<a href=".php">Finals</a>
+						</span></li>
+						<li>Open Women <span class="nav-links">
+							<a href=".php">Qualifying</a> |
+							<a href=".php">Finals</a>
+						</span></li>
+			    </ul>
+			  </div>
+			</div>
+		</div><!-- /.navbar -->
+
+
+
+
+<section class="c_scores">
+	<div class="climber" id="NW001" data-top="" data-topA="" data-bonus="" data-bonusA="">
+		<h3 class="c_name">Anna</h3>
+		<div class="c_score"></div>
+	</div>
+
+</section>
+
+
+
+		<footer class="container">
+			<p class="pull-right"><a href="#">Back to top</a></p>
+			<div class="footer-border container">
+			</div>
+		</footer>
+
+		<!-- Bootstrap core JavaScript-->
+		<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+
+
+	</body>
+</html>
