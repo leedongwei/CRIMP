7d1b74ef7a40eabf38f6380b1dcc2752f9f4ffc1
ecc-weizhi <ecc.weizhi@gmail.com>
2014-06-17 17:58:44 +0800

    Add some helper methods in Helper class.

--- Android/CRIMP/src/com/nusclimb/live/crimp/Helper.java
+++ Android/CRIMP/src/com/nusclimb/live/crimp/Helper.java
@@ -1,9 +1,114 @@
 package com.nusclimb.live.crimp;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * This class contains helper method for CRIMP. All methods must be 
+ * public static.
+ * 
+ * @author Lin Weizhi (ecc.weizhi@gmail.com)
+ *
+ */
 public class Helper {
+	/**
+	 * Pair class for storing pair of strings.
+	 */
+	public static class StringPair{
+		public String first;
+		public String second;
+		
+		public StringPair(String first, String second){
+			this.first = first;
+			this.second = second;
+		}
+	}
+	
+	// Map containing alias of names for each round.
+	private static final Map<String, StringPair> roundMap;
+    static {
+        Map<String, StringPair> aMap = new HashMap<String, StringPair>() ;
+        aMap.put("U17M Qualifier", new StringPair("U17M", "UMQ"));
+        aMap.put("U17M Final", new StringPair("U17M", "UMF"));
+        
+        aMap.put("U17W Qualifier", new StringPair("U17W", "UWQ"));
+        aMap.put("U17W Final", new StringPair("U17W", "UWF"));
+        
+        aMap.put("NM Qualifier", new StringPair("NM", "NMQ"));
+        aMap.put("NM Final", new StringPair("NM", "NMF"));
+        
+        aMap.put("NW Qualifier", new StringPair("NW", "NWQ"));
+        aMap.put("NW Final", new StringPair("NW", "NWF"));
+        
+        aMap.put("IM Qualifier", new StringPair("IM", "IMQ"));
+        aMap.put("IM Final", new StringPair("IM", "IMF"));
+        
+        aMap.put("IW Qualifier", new StringPair("IW", "IWQ"));
+        aMap.put("IW Final", new StringPair("IW", "IWF"));
+        
+        aMap.put("OM Qualifier", new StringPair("OM", "OMQ"));
+        aMap.put("OM Final", new StringPair("OM", "OMF"));
+        
+        aMap.put("OW Qualifier", new StringPair("OW", "OWQ"));
+        aMap.put("OW Final", new StringPair("OW", "OWF"));
+        
+        roundMap = Collections.unmodifiableMap(aMap);
+    }
+	
+    /**
+     * Convert to round id given full round name.
+     * 
+     * @param fullRoundName Full name of round.
+     * @return Id of round.
+     */
+	public static String toIdRound(String fullRoundName){
+		StringPair synonyms = roundMap.get(fullRoundName);
+		if(synonyms != null)
+			return synonyms.first;
+		else
+			return null;
+	}
+	
+	/**
+	 * Convert to round name suitable for server side.
+	 * 
+	 * @param fullRoundName Full name of round.
+	 * @return Name of round suitable for server.
+	 */
+	public static String toServerRound(String fullRoundName){
+		StringPair synonyms = roundMap.get(fullRoundName);
+		if(synonyms != null)
+			return synonyms.second;
+		else
+			return null;
+	}
+	
+	/**
+	 * Parse a route name to 2 digit String.
+	 * 
+	 * @param routeFullName Route name to parse.
+	 * @return Parsed route name
+	 */
+	public static String parseRoute(String routeFullName){
+		String[] temp = routeFullName.split(" ");
+		if(temp[1].length() < 2) {
+			return "0" + temp[1];
+		}
+		else{
+			return temp[1];
+		}
+	}
+	
+	/**
+	 * Helper method to convert int[] to List.
+	 * 
+	 * @param primitive int[] to convert.
+	 * @return Converted List.
+	 */
 	public static List<Integer> primitiveToList(int[] primitive){
 		List<Integer> myList = new ArrayList<Integer>();
 		
@@ -14,6 +119,12 @@ public class Helper {
 		return myList;
 	}
 	
+	/**
+	 * Helper method to convert String[] to List.
+	 * 
+	 * @param primitive String[] to convert.
+	 * @return Converted List.
+	 */
 	public static List<String> primitiveToList(String[] primitive){
 		List<String> myList = new ArrayList<String>();
 		
@@ -22,6 +133,39 @@ public class Helper {
 		}
 		
 		return myList;
+	}
+	
+	/**
+	 * Helper method to convert InputStream to String. Does not close stream.
+	 * Credit goes to http://stackoverflow.com/a/5445161/3733616
+	 * 
+	 * @param is InputStream to convert
+	 * @return String of stream content.
+	 */
+	public static String convertStreamToString(java.io.InputStream is) {
+	    java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+	    return s.hasNext() ? s.next() : "";
+	}
+	
+	// Variable for generateViewId()
+	private static final AtomicInteger sNextGeneratedId = new AtomicInteger(1);
+
+	/**
+	 * Generate a value suitable for use in {@link #setId(int)}.
+	 * This value will not collide with ID values generated at build time by aapt for R.id.
+	 *
+	 * @return a generated ID value
+	 */
+	public static int generateViewId() {
+	    for (;;) {
+	        final int result = sNextGeneratedId.get();
+	        // aapt-generated IDs have the high byte nonzero; clamp to the range under that.
+	        int newValue = result + 1;
+	        if (newValue > 0x00FFFFFF) newValue = 1; // Roll over to 1, not 0.
+	        if (sNextGeneratedId.compareAndSet(result, newValue)) {
+	            return result;
+	        }
+	    }
 	}
 
 }
