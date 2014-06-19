b4c897d4a1ffb035dc5e53f67032c3f4de0125b5
ecc-weizhi <ecc.weizhi@gmail.com>
2014-06-19 12:06:02 +0800

    Added Jackson POJO for climber info.

---
+++ Android/CRIMP/src/com/nusclimb/live/crimp/json/RoundInfoMap.java
@@ -0,0 +1,15 @@
+package com.nusclimb.live.crimp.json;
+
+import java.util.HashMap;
+
+/**
+ * A convenience class to map the content of RoundInfo so that we can use
+ * hashing to retrieve values.
+ * 
+ * @author Zhi
+ *
+ */
+@SuppressWarnings("serial")
+public class RoundInfoMap extends HashMap<String, String>{
+	
+}
