abfa3dcfb74f1d8212fa2344bc6730ccb07a4ee0
Si Ling <siling1992@gmail.com>
2015-06-30 16:58:34 +0800

    Added LoginResponse class update LoginRequest class to follow API

--- judge/android/CRIMP/app/src/main/java/com/nusclimb/live/crimp/common/spicerequest/LoginRequest.java
+++ judge/android/CRIMP/app/src/main/java/com/nusclimb/live/crimp/common/spicerequest/LoginRequest.java
@@ -2,8 +2,18 @@ package com.nusclimb.live.crimp.common.spicerequest;
 
 import android.util.Log;
 
+import com.nusclimb.live.crimp.common.json.LoginResponse;
 import com.nusclimb.live.crimp.common.json.Session;
 import com.octo.android.robospice.request.springandroid.SpringAndroidSpiceRequest;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Spice request for score of a climber for a route.
@@ -11,40 +21,48 @@ import com.octo.android.robospice.request.springandroid.SpringAndroidSpiceReques
  * @author Lin Weizhi (ecc.weizhi@gmail.com)
  *
  */
-public class LoginRequest extends SpringAndroidSpiceRequest<Session> {
+public class LoginRequest extends SpringAndroidSpiceRequest<LoginResponse> {
     private static final String TAG = LoginRequest.class.getSimpleName();
-    private static final String BASE_URL = "http://httpbin.org/";
+    private static final String BASE_URL = "http://crimp-testing-0625.meteor.com/api/judge/login";
 
+    // Information needed to craft a LoginRequest
     private String accessToken;
+    private String expiresAt;
 
     /**
      *
      *
      * @param accessToken
      */
-    public LoginRequest(String accessToken) {
-        super(Session.class);
+    public LoginRequest(String accessToken, String expiresAt) {
+        super(LoginResponse.class);
         this.accessToken = accessToken;
+        this.expiresAt = expiresAt;
     }
 
     @Override
-    public Session loadDataFromNetwork() throws Exception {
+    public LoginResponse loadDataFromNetwork() throws Exception {
         // Craft URL.
-        String address = BASE_URL+"get";
+        String address = BASE_URL;
+
+        // Prepare message
+        Map<String, String> parameters = new HashMap<String, String>();
+        parameters.put("accessToken", accessToken);
+        parameters.put("expiresAt", expiresAt);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<Map<String,String>> request = new HttpEntity<Map<String, String>>(parameters, headers);
 
         // Actual network calls.
-        Session content = getRestTemplate().getForObject(address, Session.class);
+        LoginResponse content = getRestTemplate().postForObject(address, request, LoginResponse.class);
 
         Log.v(TAG, "Address=" + address + "\ncontent=" + content.toString());
 
         return content;
     }
 
-    public String getAccessToken(){
-        return accessToken;
-    }
-
     public String createCacheKey() {
-        return accessToken;
+        return accessToken+expiresAt;
     }
 }
