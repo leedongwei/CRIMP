d97710ffee962ea748eb6de1a9a764fefac4bc47
DongWei <leedongwei@gmail.com>
2015-07-05 14:23:25 -0700

    cleaning up publications

--- meteor/crimp-server/server/publications.js
+++ meteor/crimp-server/server/publications.js
@@ -1,19 +1,45 @@
 // TODO: Add fine-grained permissions here so we do not overexpose
 // extra information to unauthorized users
 Meteor.publish('getCategories', function() {
-  return Categories.find({});
+  return Categories.find({}, {
+    fields: {
+      _id: 1,
+      name: 1,
+      category_id: 1,
+      scores_finalized: 1,
+      time_start: 1,
+      time_end: 1
+    }
+  });
 });
 
 Meteor.publish('getClimbers', function(category) {
   category = typeof category !== 'undefined' ? category : {};
-  return Climbers.find(category);
+  return Climbers.find(category, {
+    fields: {
+      number: 0
+    }
+  });
 });
 
 Meteor.publish('getScores', function(category) {
   category = typeof category !== 'undefined' ? category : {};
-  return Scores.find(category);
+  return Scores.find(category, {
+    fields: {
+      category_id: 0,
+      unique_id: 0,
+      admin_id: 0,
+      updated_at: 0
+    }
+  });
 });
 
 Meteor.publish('getActiveClimbers', function() {
-  return ActiveClimbers.find({});
+  return ActiveClimbers.find({}, {
+    fields: {
+      route_id: 1,
+      climber_id: 1,
+      climber_name: 1
+    }
+  });
 });
