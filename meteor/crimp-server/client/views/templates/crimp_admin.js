bf9e7fae4549568f8f7e2e3c8fd6d04d1ae920fa
DongWei <leedongwei@gmail.com>
2015-07-19 18:29:24 -0700

    overflow for showing scores on dashboard

--- meteor/crimp-server/client/views/templates/crimp_admin.js
+++ meteor/crimp-server/client/views/templates/crimp_admin.js
@@ -4,7 +4,7 @@ Template.crimp_admin.onCreated(function() {
   // Shorten strings used by Moment.js
   moment.locale('en', {
     relativeTime : {
-      future: "in %s",  past:   "%s ago",
+      future: "in %s",  past:   "%s",
       s:  "%dsec",
       m:  "a min",      mm: "%dmin",
       h:  "an hr",      hh: "%dhrs",
@@ -39,7 +39,9 @@ Template.admin_dashboard.onCreated(function() {
 
 Template.admin_dashboard.helpers({
   adminActiveClimbers: function() {
-    return ActiveClimbers.find({}).fetch();
+    return ActiveClimbers.find({})
+            .fetch()
+            .sort(adminActiveClimberSort);
   },
   adminPendingJudges: function() {
     return Roles.getUsersInRole('pending');
@@ -83,6 +85,9 @@ Template.admin_database.helpers({
 /*
  * Utility functions
  */
+function adminActiveClimberSort (a, b) {
+  return a.route_id > b.route_id ? 1 : -1;
+}
 function recentScoreSort(a, b) {
   return a.updated_at >= b.updated_at ? -1 : 1;
 }
