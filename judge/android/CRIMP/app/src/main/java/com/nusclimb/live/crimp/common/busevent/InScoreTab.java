660a2beeeb95b77b05314d492bde5a6e59a355f0
Lin Weizhi <ecc.weizhi@gmail.com>
2015-12-15 11:27:40 +0800

    Remove all Bus related object.

--- judge/android/CRIMP/app/src/main/java/com/nusclimb/live/crimp/common/busevent/InScoreTab.java
+++
@@ -1,8 +0,0 @@
-package com.nusclimb.live.crimp.common.busevent;
-
-/**
- * Created by weizhi on 15/7/2015.
- */
-public class InScoreTab {
-    private final String TAG = InScoreTab.class.getSimpleName();
-}
--- judge/android/CRIMP/app/src/main/java/com/nusclimb/live/crimp/common/busevent/ScanOnResume.java
+++
@@ -1,8 +0,0 @@
-package com.nusclimb.live.crimp.common.busevent;
-
-/**
- * Created by Zhi on 7/12/2015.
- */
-public class ScanOnResume {
-    private final String TAG = ScanOnResume.class.getSimpleName();
-}
