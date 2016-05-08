1da558b8d7d6e21b6b80ac9bae70ca97e62fabaa
DongWei <leedongwei@gmail.com>
2016-05-08 23:02:19 +0800

    Outline for Categories collection

--- meteor/crimp-server/imports/data/categories.js
+++ meteor/crimp-server/imports/data/categories.js
@@ -4,8 +4,8 @@ import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 
 
 class CategoriesCollection extends Mongo.collection {
-  insert(message, callback) {
-    return super.insert(message, callback);
+  insert() {
+    return false;
   }
   update() {
     return false;
@@ -14,3 +14,65 @@ class CategoriesCollection extends Mongo.collection {
     return false;
   }
 }
+
+export const Categories = new CategoriesCollection('Categories');
+Categories.schema = new SimpleSchema({
+  category_id: {
+    type: String,
+  },
+  category_name: {
+    type: String,
+  },
+  acronym: {
+    type: String,
+    label: '3 char acronym of category',
+    min: 3,
+    max: 3,
+  },
+  routes: {
+    type: [Object],
+    label: 'List of all the routes in category',
+  },
+  'routes.$.route_id': {
+    type: String,
+  },
+  'routes.$.route_name': {
+    type: String,
+  },
+
+  // TODO: DongWei
+  'routes.$.whatever': {
+    type: String,
+  },
+  score_finalized: {
+    type: Boolean,
+    label: 'Confirm scores for category',
+  },
+  time_start: {
+    type: Date,
+    label: 'Starting time of category',
+  },
+  time_end: {
+    type: Date,
+    label: 'Starting time of category',
+  },
+  updated_at: {
+    type: Date,
+    optional: true,   // optional to pass ValidatedMethod
+    autoValue: () => new Date(),
+  },
+});
+Categories.attachSchema(Categories.schema);
+
+if (ENVIRONMENT.NODE_ENV === 'production') {
+  console.log('INSIDE ENVIRONMENT');
+  Categories.deny({
+    insert() { return true; },
+    update() { return true; },
+    remove() { return true; },
+  });
+}
+
+
+Categories.methods = {};
+//Categories.methods.insert =
