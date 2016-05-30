500185ba3b5d23ddb3f222ddba4df0005afc9e32
DongWei <leedongwei@gmail.com>
2016-05-30 23:14:05 +0800

    Added interval function for ActiveTracker

--- meteor/crimp-server/imports/data/activetracker.js
+++ meteor/crimp-server/imports/data/activetracker.js
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 import { ValidatedMethod } from 'meteor/mdg:validated-method';
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
@@ -34,8 +35,8 @@ ActiveTracker.schema = new SimpleSchema({
   user_expiry: {
     type: Date,
     label: 'Time to delete document',
-    // expires in 10mins
-    autoValue: () => new Date(Date.now() + (15 * 60000)),
+    // expires in 20mins
+    autoValue: () => new Date(Date.now() + (20 * 60000)),
   },
   climber_id: {
     type: String,
@@ -116,3 +117,57 @@ ActiveTracker.methods = {};
 
 
 export default ActiveTracker;
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *  Interval-based functions                               *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/**
+ *  Checks admin and climber values to see if they are expired and remove
+ *  them from the document
+ *
+ *  @param
+ *    {object} ac - 1 document from ActiveMonitor
+ */
+function removeExpiredActiveTracker(trackerDoc) {
+  const timeNow = Date.now();
+
+  // Delete entire document if judge has expired
+  if (trackerDoc.user_expiry < timeNow) {
+    ActiveTracker.remove({ route_id: trackerDoc.route_id }, () => {});
+    return;
+  }
+
+  // Remove climber's name if climber has expired
+  // Skip update if there is no climber on the document
+  if (trackerDoc.climber_id &&
+      trackerDoc.climber_expiry < timeNow) {
+    ActiveTracker.update(
+      { route_id: trackerDoc.route_id },
+      { $set: {
+        climber_id: '',
+        marker_id: '',
+        climber_name: '',
+      } },
+      () => {}
+    );
+
+    return;
+  }
+}
+
+
+/**
+ *  Find and remove any expired values in ActiveClimbers at a set interval
+ */
+export function runActiveTracker() {
+  Meteor.setInterval(() => {
+    // TODO: Remove console.log
+    console.log(`AT:  ${new Date(Date.now())}`);
+
+    ActiveTracker.find({})
+                 .forEach(removeExpiredActiveTracker);
+  }, 30 * 1000);
+}
+
