9360e773db420a65ec1403a4a6ad17c129eeb7cc
Lin Weizhi <ecc.weizhi@gmail.com>
2016-06-16 15:57:05 +0800

    - fix NPE with reading errorBody. - Added more fabric logging.

--- android/app/src/main/java/rocks/crimp/crimp/service/RestRequestTask.java
+++ android/app/src/main/java/rocks/crimp/crimp/service/RestRequestTask.java
@@ -2,6 +2,7 @@ package rocks.crimp.crimp.service;
 
 import android.content.Intent;
 
+import com.crashlytics.android.Crashlytics;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -53,57 +54,145 @@ public class RestRequestTask implements Task<RestRequestTask.Callback> {
                 case CrimpService.ACTION_GET_CATEGORIES:
                     response = CrimpApplication.getCrimpWS().getCategories();
                     if(!response.isSuccessful()){
-                        errorBody = response.errorBody().string();
+                        if(response.errorBody() != null){
+                            errorBody = response.errorBody().string();
+                        }
+                        else{
+                            Crashlytics.log("response.errorBody() is null in get categories");
+                            Crashlytics.logException(new Exception("response.errorBody is null in get categories. raw: "+response.raw()));
+                        }
                         Timber.e("Get categories error response: %s", errorBody);
+
+                        if(errorBody == null){
+                            Crashlytics.log("response.errorBody is null in get categories. raw: "+response.raw());
+                            Crashlytics.logException(new Exception("response.errorBody is null in get categories. raw: "+response.raw()));
+                        }
                     }
                     break;
                 case CrimpService.ACTION_GET_SCORE:
                     response = CrimpApplication.getCrimpWS().getScore(requestBean);
                     if(!response.isSuccessful()){
-                        errorBody = response.errorBody().string();
+                        if(response.errorBody() != null){
+                            errorBody = response.errorBody().string();
+                        }
+                        else{
+                            Crashlytics.log("response.errorBody is null in get score. raw: "+response.raw());
+                            Crashlytics.logException(new Exception("response.errorBody is null in get score. raw: "+response.raw()));
+                        }
                         Timber.e("Get score error response: %s", errorBody);
+
+                        if(errorBody == null){
+                            Crashlytics.log("errorBody is null in get score. raw: "+response.raw());
+                            Crashlytics.logException(new Exception("errorBody is null in get score. raw: "+response.raw()));
+                        }
                     }
                     break;
                 case CrimpService.ACTION_SET_ACTIVE:
                     response = CrimpApplication.getCrimpWS().setActive(requestBean);
                     if(!response.isSuccessful()){
-                        errorBody = response.errorBody().string();
+                        if(response.errorBody() != null){
+                            errorBody = response.errorBody().string();
+                        }
+                        else{
+                            Crashlytics.log("response.errorBody is null in set active. raw: "+response.raw());
+                            Crashlytics.logException(new Exception("response.errorBody is null in set active. raw: "+response.raw()));
+                        }
                         Timber.e("Set active error response: %s", errorBody);
+
+                        if(errorBody == null){
+                            Crashlytics.log("errorBody is null in set active. raw: "+response.raw());
+                            Crashlytics.logException(new Exception("errorBody is null in set active. raw: "+response.raw()));
+                        }
                     }
                     break;
                 case CrimpService.ACTION_CLEAR_ACTIVE:
                     response = CrimpApplication.getCrimpWS().clearActive(requestBean);
                     if(!response.isSuccessful()){
-                        errorBody = response.errorBody().string();
+                        if(response.errorBody() != null){
+                            errorBody = response.errorBody().string();
+                        }
+                        else{
+                            Crashlytics.log("response.errorBody is null in clear active. raw: "+response.raw());
+                            Crashlytics.logException(new Exception("response.errorBody is null in clear active. raw: "+response.raw()));
+                        }
                         Timber.e("Clear active error response: %s", errorBody);
+
+                        if(errorBody == null){
+                            Crashlytics.log("errorBody is null in clear active. raw: "+response.raw());
+                            Crashlytics.logException(new Exception("errorBody is null in clear active. raw: "+response.raw()));
+                        }
                     }
                     break;
                 case CrimpService.ACTION_LOGIN:
                     response = CrimpApplication.getCrimpWS().login(requestBean);
                     if(!response.isSuccessful()){
-                        errorBody = response.errorBody().string();
+                        if(response.errorBody() != null){
+                            errorBody = response.errorBody().string();
+                        }
+                        else{
+                            Crashlytics.log("response.errorBody is null in login. raw: "+response.raw());
+                            Crashlytics.logException(new Exception("response.errorBody is null in login. raw: "+response.raw()));
+                        }
                         Timber.e("Login error response: %s", errorBody);
+
+                        if(errorBody == null){
+                            Crashlytics.log("errorBody is null in login. raw: "+response.raw());
+                            Crashlytics.logException(new Exception("errorBody is null in login. raw: "+response.raw()));
+                        }
                     }
                     break;
                 case CrimpService.ACTION_REPORT_IN:
                     response = CrimpApplication.getCrimpWS().reportIn(requestBean);
                     if(!response.isSuccessful()){
-                        errorBody = response.errorBody().string();
+                        if(response.errorBody() != null){
+                            errorBody = response.errorBody().string();
+                        }
+                        else{
+                            Crashlytics.log("response.errorBody is null in report in. raw: "+response.raw());
+                            Crashlytics.logException(new Exception("response.errorBody is null in report in. raw: "+response.raw()));
+                        }
                         Timber.e("Report error response: %s", errorBody);
+
+                        if(errorBody == null){
+                            Crashlytics.log("errorBody is null in report in. raw: "+response.raw());
+                            Crashlytics.logException(new Exception("errorBody is null in report in. raw: "+response.raw()));
+                        }
                     }
                     break;
                 case CrimpService.ACTION_REQUEST_HELP:
                     response = CrimpApplication.getCrimpWS().requestHelp(requestBean);
                     if(!response.isSuccessful()){
-                        errorBody = response.errorBody().string();
+                        if(response.errorBody() != null){
+                            errorBody = response.errorBody().string();
+                        }
+                        else{
+                            Crashlytics.log("response.errorBody is null in request help. raw: "+response.raw());
+                            Crashlytics.logException(new Exception("errorBody is null in request help. raw: "+response.raw()));
+                        }
                         Timber.e("Help me error response: %s", errorBody);
+
+                        if(errorBody == null){
+                            Crashlytics.log("errorBody is null in request help. raw: "+response.raw());
+                            Crashlytics.logException(new Exception("errorBody is null in request help. raw: "+response.raw()));
+                        }
                     }
                     break;
                 case CrimpService.ACTION_LOGOUT:
                     response = CrimpApplication.getCrimpWS().logout(requestBean);
                     if(!response.isSuccessful()){
-                        errorBody = response.errorBody().string();
+                        if(response.errorBody() != null){
+                            errorBody = response.errorBody().string();
+                        }
+                        else{
+                            Crashlytics.log("response.errorBody is null in logout. raw: "+response.raw());
+                            Crashlytics.logException(new Exception("response.errorBody is null in logout. raw: "+response.raw()));
+                        }
                         Timber.e("Logout error response: %s", errorBody);
+
+                        if(errorBody == null){
+                            Crashlytics.log("errorBody is null in logout. raw: "+response.raw());
+                            Crashlytics.logException(new Exception("errorBody is null in logout. raw: "+response.raw()));
+                        }
                     }
                     break;
                 default:
@@ -122,15 +211,23 @@ public class RestRequestTask implements Task<RestRequestTask.Callback> {
             Timber.e("Failed to receive correct response from server");
             ObjectMapper objectMapper = new ObjectMapper();
             ErrorJs errorJs = null;
-            try {
-                errorJs = objectMapper.readValue(errorBody, ErrorJs.class);
-            } catch (JsonParseException e) {
-                e.printStackTrace();
-            } catch (JsonMappingException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
+
+            // Hot fix for when errorBody is null.
+            if(errorBody != null){
+                try {
+                    errorJs = objectMapper.readValue(errorBody, ErrorJs.class);
+                } catch (JsonParseException e) {
+                    Crashlytics.logException(e);
+                    e.printStackTrace();
+                } catch (JsonMappingException e) {
+                    Crashlytics.logException(e);
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    Crashlytics.logException(e);
+                    e.printStackTrace();
+                }
             }
+
             callback.onRestFailure(txId, errorJs);
         }
     }
