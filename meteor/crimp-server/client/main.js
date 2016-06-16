770b83b8f1e78cc33cb7244131c693c7eed97c1b
DongWei <leedongwei@gmail.com>
2016-06-16 09:19:16 +0800

    Allow variables on Global

--- meteor/crimp-server/client/main.js
+++ meteor/crimp-server/client/main.js
@@ -9,20 +9,26 @@ import Categories from '../imports/data/categories';
 import Teams from '../imports/data/teams';
 import Climbers from '../imports/data/climbers';
 import Scores from '../imports/data/scores';
-// import HelpMe from '../imports/data/helpme';
-// import ActiveTracker from '../imports/data/activetracker';
+import HelpMe from '../imports/data/helpme';
+import ActiveTracker from '../imports/data/activetracker';
 
 import IFSC_TB from '../imports/score_systems/ifsc-top-bonus.js';
 import TFBb from '../imports/score_systems/top-flash-bonus2-bonus1';
 import CRIMP from '../imports/settings';
 
+function makeGlobal(key, variable) {
+  if (typeof window !== 'undefined') { window[key] = variable; }
+  if (typeof global !== 'undefined') { global[key] = variable; }
+}
 
-window.Events = Events;
-window.Categories = Categories;
-window.Teams = Teams;
-window.Climbers = Climbers;
-window.Scores = Scores;
-window.CRIMP = CRIMP;
+makeGlobal('Events', Events);
+makeGlobal('Categories', Categories);
+makeGlobal('Teams', Teams);
+makeGlobal('Climbers', Climbers);
+makeGlobal('Scores', Scores);
+makeGlobal('HelpMe', HelpMe);
+makeGlobal('ActiveTracker', ActiveTracker);
+makeGlobal('CRIMP', CRIMP);
 
 
 // Utility templates
