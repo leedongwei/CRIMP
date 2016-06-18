e4d80bd823f9ad9bc7b459578071cc8b08d7f25b
DongWei <leedongwei@gmail.com>
2016-06-18 16:07:12 +0800

    Turning Kadira back on

--- meteor/crimp-server/server/kadira.js
+++ meteor/crimp-server/server/kadira.js
@@ -1,3 +1,3 @@
-// import { Kadira } from 'meteor/meteorhacks:kadira';
+import { Kadira } from 'meteor/meteorhacks:kadira';
 
-// Kadira.connect('RpusNHkNvwn4uvh3D', 'ecd51a40-4e96-4014-8d49-750a3108ceab');
+Kadira.connect('RpusNHkNvwn4uvh3D', 'ecd51a40-4e96-4014-8d49-750a3108ceab');
